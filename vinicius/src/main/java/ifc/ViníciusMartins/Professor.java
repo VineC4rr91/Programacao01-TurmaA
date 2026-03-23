99064d4600a3155d2ec623036178a961539ca28c
suporte <suporte@191.52.13.184>
2026-03-23 13:58:29 -0300

    Talvez a última versão!!! 0.7

---
+++ vinicius/src/main/java/ifc/ViníciusMartins/Professor.java
@@ -0,0 +1,102 @@
+package ifc.ViníciusMartins;
+
+import java.util.Date;
+
+/*
+ Feito por VineC4rr91
+ */
+public class Professor {
+    private String nivelEnsino;
+    private String nomeCompleto;
+    private int siape; 
+    private String email;
+    private int telefoneContato;
+    private String titulacaoAcademica;
+    private String areaAtuacao;
+    private String regimeTrabalho;
+    private Date dataIngreso;
+    private String formacao;
+////////////////////////////// 
+    public Professor (){
+    }
+    
+    public String getNivelEnsino (){
+        return this.nivelEnsino;
+    }
+            
+    public void setNivelEnsino (String _nivelEnsino){
+        this.nivelEnsino = _nivelEnsino;
+    }
+    
+    public String getNomeCompleto (){
+        return this.nomeCompleto;
+    }
+            
+    public void setNomeCompleto (String _nomeCompleto){
+        this.nomeCompleto = _nomeCompleto;
+    }
+    
+    public int getSiape (){
+        return this.siape;
+    }
+            
+    public void setNomeCompleto (int _siape){
+        this.siape = _siape;
+    }    
+        
+    public String getEmail (){
+        return this.email;
+    }
+            
+    public void setEmail (String _email){
+        this.email = _email;
+    }
+    
+    public int getTelefoneContato (){
+        return this.telefoneContato;
+    }
+            
+    public void setTelefoneContato (int _telefoneContato){
+        this.telefoneContato = _telefoneContato;
+    }    
+        
+    public String getTitulacaoAcademica (){
+        return this.titulacaoAcademica;
+    }
+            
+    public void setTitulacaoAcademica (String _titulacaoAcademica){
+        this.titulacaoAcademica = _titulacaoAcademica;
+    }
+   
+    public String getAreaAtuacao (){
+        return this.areaAtuacao;
+    }
+            
+    public void setAreaAtuacao (String _areaAtuacao){
+        this.areaAtuacao = _areaAtuacao;
+    }
+    
+    public String getRegimeTrabalho (){
+        return this.regimeTrabalho;
+    }
+            
+    public void setRegimeTrabalho (String _regimeTrabalho){
+        this.regimeTrabalho = _regimeTrabalho;
+    }
+    
+    public Date getDataIngresso (){
+        return this.dataIngreso;
+    }
+            
+    public void setDataIngresso (Date _dataIngreso){
+        this.dataIngreso = _dataIngreso;
+    }
+    
+    public String getFormacao (){
+        return this.formacao;
+    }
+            
+    public void setFormacao (String _formacao){
+        this.formacao = _formacao;
+    }
+}
---
+++ vinicius/src/main/java/ifc/ViníciusMartins/Turma.java
@@ -0,0 +1,44 @@
+package ifc.ViníciusMartins;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/*
+    Feito por VineC4rr91
+ */
+public class Turma {
+    private String nome;
+    private String ano;
+    private String curso;
+    ///////////////////
+    public Turma(){
+    }
+    
+    public String getNome (){
+        return this.nome;
+    }
+            
+    public void setNome (String _nome){
+        this.nome = _nome;
+    }
+    
+    public String getAno (){
+    return this.ano;
+    }
+            
+    public void setAno (String _ano){
+        this.ano = _ano;    
+    }
+    
+    public String getCurso (){
+        return this.curso;
+    }
+            
+    public void setCurso (String _curso){
+        this.curso = _curso;
+    }
+
+    Set<Estudante> discentes = new HashSet<Estudante>();
+    Set<Disciplina> discipina = new HashSet<Disciplina>();
+    Turma professor = new Turma();
+}
